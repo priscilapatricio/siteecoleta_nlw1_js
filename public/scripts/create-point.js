f6f8853ad5ecf2a23660cdadbc201bcde4fa2cde
Priscila Patricio <priscilapatricio2@gmail.com>
2020-07-10 16:51:13 -0300

    Site finalizado

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -68,7 +68,7 @@ function handSelectedItem(event){
     
     const itemId = itemLi.dataset.id
 
-    
+    // console.log('ITEM ID: ', itemId)
 
     //verificar se existem itens selecionados, se sim
     //pegar os itens selecionados
@@ -92,6 +92,8 @@ function handSelectedItem(event){
         //adicionar à seleção
         selectedItems.push(itemId)
    }
+
+    //  console.log('selectedItems: ', selectedItems)
 
      //atualizar o campo escondido com os itens selecionados
      collectedItems.value = selectedItems
